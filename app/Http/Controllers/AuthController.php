6626bf49279ff0c587b779645468df8214dde497
Ivan <ivan.todoric1@gmail.com>
2026-03-31 23:15:53 +0200

    Clean project structure, remove old MVC

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLogin()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        // Za početak samo test
+        return "Login POST radi!";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,4 @@
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
